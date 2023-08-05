f44f58892108927436b4a4f7074edd03ba30c46a
Daniel Connelly <dhconnelly@gmail.com>
2023-08-05 15:35:32 +0200

    year21day16 stubs

---
+++ src/main/scala/adventofcode/Year21Day16.scala
@@ -0,0 +1,6 @@
+package adventofcode
+
+object Year21Day16 {
+  def main(args: Array[String]): Unit =
+    println("Hello, world")
+}
---
+++ src/test/scala/adventofcode/Year21Day16Test.scala
@@ -0,0 +1,9 @@
+package adventofcode
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Year21Day16Test extends AnyFunSuite {
+  test("foo") {
+    assert(true)
+  }
+}
