de95112eaa1c0f77ae0f36464f65b46cd5085287
Daniel Connelly <dhconnelly@gmail.com>
2023-08-08 00:06:37 +0200

    aoc21day16: parse bitstring

--- src/main/scala/adventofcode/Year2021Day16.scala
+++ src/main/scala/adventofcode/Year2021Day16.scala
@@ -1,6 +1,29 @@
 package adventofcode
 
+import scala.io.Source
+import scala.util.{Try, Success, Failure}
+
 object Year2021Day16 {
-  def main(args: Array[String]): Unit =
-    println("Hello, world")
+  // takes an 8-bit unsigned value and returns its big-endian bitstring
+  def toBits(b: Int): Seq[Byte] =
+    (0 to 7)
+      .map(i => (b & (1 << i)) > 0)
+      .map(b => if (b) 1 else 0)
+      .map(_.toByte)
+      .reverse
+
+  // returns a bitstring given a hexstring
+  def parse(src: Source): Try[Seq[Byte]] = {
+    src.filter(_ != '\n').grouped(2).foldLeft(Try(Seq.empty[Byte])) {
+      (result, pair) =>
+        result.flatMap(acc =>
+          Try(Integer.parseInt(pair.mkString, 16)).map(b => acc ++ toBits(b))
+        )
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val data = parse(Source.fromFile(args(0))).get
+    println(data)
+  }
 }
--- src/test/scala/adventofcode/Year2021Day16Test.scala
+++ src/test/scala/adventofcode/Year2021Day16Test.scala
@@ -1,9 +1,34 @@
 package adventofcode
 
 import org.scalatest.funsuite.AnyFunSuite
+import adventofcode.Year2021Day16._
+import scala.io.Source
+import scala.util.{Success, Try}
 
 class Year2021Day16Test extends AnyFunSuite {
-  test("foo") {
-    assert(true)
+  def bits(s: String): Seq[Byte] = s.map(_ - '0').map(_.toByte)
+  def parse(s: String): Try[Seq[Byte]] =
+    Year2021Day16.parse(Source.fromString(s))
+
+  test("parse") {
+    assert(parse("D2FE28").get == bits("110100101111111000101000"))
+    assert(
+      parse("38006F45291200").get == bits(
+        "00111000000000000110111101000101001010010001001000000000"
+      )
+    )
+    assert(
+      parse("EE00D40C823060").get == bits(
+        "11101110000000001101010000001100100000100011000001100000"
+      )
+    )
+  }
+
+  test("parse.newline") {
+    assert(parse("D2FE28\n").get == bits("110100101111111000101000"))
+  }
+
+  test("parse.invalid") {
+    assert(parse("foo").isFailure)
   }
 }
