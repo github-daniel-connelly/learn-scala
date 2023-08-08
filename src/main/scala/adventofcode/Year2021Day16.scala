f9b649a91385ae9b486661d6ebea49b1f14d9c1a
Daniel Connelly <daniel.connelly@teads.com>
2023-08-08 14:47:50 +0200

    aoc21day16: parse literals

--- src/main/scala/adventofcode/Year2021Day16.scala
+++ src/main/scala/adventofcode/Year2021Day16.scala
@@ -3,9 +3,41 @@ package adventofcode
 import scala.io.Source
 import scala.util.{Try, Success, Failure}
 
+sealed abstract class Packet(val version: Int)
+case class Literal(override val version: Int, lit: Int) extends Packet(version)
+
 object Year2021Day16 {
+  type Bit = Byte
+
+  def decodeInt(b: Seq[Bit]): Int =
+    b.foldLeft(0)((acc, b) => (acc << 1) | b)
+
+  def parseLiteral(version: Int, b: Seq[Bit]): (Packet, Seq[Bit]) = {
+    val groups = b.grouped(5).toSeq
+    val prefix = groups.takeWhile(group => group.head == 1)
+    val suffix = groups.drop(prefix.length).take(1)
+    val bits = (prefix ++ suffix).flatMap(_.drop(1))
+    val int = decodeInt(bits)
+    (Literal(version, int), b.drop(5 * bits.length))
+  }
+
+  def parseHeader(b: Seq[Bit]): Try[(Int, Int, Seq[Bit])] = Try {
+    val Seq(v1, v2, v3) = b.take(3)
+    val Seq(t1, t2, t3) = b.drop(3).take(3)
+    (v1 << 2 | v2 << 1 | v3, t1 << 2 | t2 << 1 | t3, b.drop(6))
+  }
+
+  def parsePacket(b: Seq[Bit]): Try[(Packet, Seq[Bit])] =
+    parseHeader(b).map(result =>
+      result._2 match {
+        case 4 => parseLiteral(result._1, result._3)
+      }
+    )
+
+  def parse(b: Seq[Bit]): Try[Packet] = parsePacket(b).map(_._1)
+
   // takes an 8-bit unsigned value and returns its big-endian bitstring
-  def toBits(b: Int): Seq[Byte] =
+  def toBits(b: Int): Seq[Bit] =
     (0 to 7)
       .map(i => (b & (1 << i)) > 0)
       .map(b => if (b) 1 else 0)
@@ -13,8 +45,8 @@ object Year2021Day16 {
       .reverse
 
   // returns a bitstring given a hexstring
-  def parse(src: Source): Try[Seq[Byte]] = {
-    src.filter(_ != '\n').grouped(2).foldLeft(Try(Seq.empty[Byte])) {
+  def decode(src: Source): Try[Seq[Bit]] = {
+    src.filter(_ != '\n').grouped(2).foldLeft(Try(Seq.empty[Bit])) {
       (result, pair) =>
         result.flatMap(acc =>
           Try(Integer.parseInt(pair.mkString, 16)).map(b => acc ++ toBits(b))
@@ -23,7 +55,7 @@ object Year2021Day16 {
   }
 
   def main(args: Array[String]): Unit = {
-    val data = parse(Source.fromFile(args(0))).get
+    val data = decode(Source.fromFile(args(0))).get
     println(data)
   }
 }
--- src/test/scala/adventofcode/Year2021Day16Test.scala
+++ src/test/scala/adventofcode/Year2021Day16Test.scala
@@ -7,28 +7,36 @@ import scala.util.{Success, Try}
 
 class Year2021Day16Test extends AnyFunSuite {
   def bits(s: String): Seq[Byte] = s.map(_ - '0').map(_.toByte)
-  def parse(s: String): Try[Seq[Byte]] =
-    Year2021Day16.parse(Source.fromString(s))
+  def decode(s: String) = Year2021Day16.decode(Source.fromString(s))
+  def parse(s: String) = Year2021Day16.parse(decode(s).get)
 
-  test("parse") {
-    assert(parse("D2FE28").get == bits("110100101111111000101000"))
+  test("decode") {
+    assert(decode("D2FE28").get == bits("110100101111111000101000"))
     assert(
-      parse("38006F45291200").get == bits(
+      decode("38006F45291200").get == bits(
         "00111000000000000110111101000101001010010001001000000000"
       )
     )
     assert(
-      parse("EE00D40C823060").get == bits(
+      decode("EE00D40C823060").get == bits(
         "11101110000000001101010000001100100000100011000001100000"
       )
     )
   }
 
-  test("parse.newline") {
-    assert(parse("D2FE28\n").get == bits("110100101111111000101000"))
+  test("decode.newline") {
+    assert(decode("D2FE28\n").get == bits("110100101111111000101000"))
+  }
+
+  test("decode.invalid") {
+    assert(decode("foo").isFailure)
   }
 
   test("parse.invalid") {
-    assert(parse("foo").isFailure)
+    assert(Year2021Day16.parse(bits("10")).isFailure)
+  }
+
+  test("parse.literal") {
+    assert(parse("D2FE28").get == Literal(6, 2021))
   }
 }
