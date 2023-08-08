f713a8c66abb0dcbf90d89474f6e96f08361f83a
Daniel Connelly <daniel.connelly@teads.com>
2023-08-08 17:09:53 +0200

    long!

--- src/main/scala/adventofcode/Year2021Day16.scala
+++ src/main/scala/adventofcode/Year2021Day16.scala
@@ -3,22 +3,37 @@ package adventofcode
 import scala.io.Source
 import scala.util.{Try, Success, Failure}
 
-sealed abstract class Packet(val version: Int, val typ: Int)
-case class Literal(override val version: Int, lit: Int)
-    extends Packet(version, 4)
+sealed abstract class Packet(val version: Long, val typ: Long)
+case class Literal(override val version: Long, lit: Long)
+    extends Packet(version, 4) {
+  override def toString = s"$lit"
+}
 case class Operator(
-    override val version: Int,
-    override val typ: Int,
+    override val version: Long,
+    override val typ: Long,
     operands: Seq[Packet]
-) extends Packet(version, typ)
+) extends Packet(version, typ) {
+  override def toString: String = {
+    val op = typ match {
+      case 0 => "+"
+      case 1 => "*"
+      case 2 => "min"
+      case 3 => "max"
+      case 5 => ">"
+      case 6 => "<"
+      case 7 => "="
+    }
+    s"($op ${operands.map(_.toString).mkString(" ")})"
+  }
+}
 
 object Year2021Day16 {
   type Bit = Byte
 
-  private def decodeInt(b: Seq[Bit]): Int =
-    b.foldLeft(0)((acc, b) => (acc << 1) | b)
+  private def decodeInt(b: Seq[Bit]): Long =
+    b.foldLeft(0L)((acc, b) => (acc << 1) | b)
 
-  private def parseLiteral(version: Int, b: Seq[Bit]): (Packet, Seq[Bit]) = {
+  private def parseLiteral(version: Long, b: Seq[Bit]): (Packet, Seq[Bit]) = {
     val groups = b.grouped(5).toSeq
     val prefix = groups.takeWhile(group => group.head == 1)
     val suffix = groups.drop(prefix.length).take(1)
@@ -38,7 +53,7 @@ object Year2021Day16 {
 
   private def parsePacketsN(
       b: Seq[Bit],
-      n: Int
+      n: Long
   ): Try[(Seq[Packet], Seq[Bit])] = {
     if (n == 0) Success((Seq.empty, b))
     else
@@ -49,8 +64,8 @@ object Year2021Day16 {
   }
 
   private def parseOperator(
-      version: Int,
-      typ: Int,
+      version: Long,
+      typ: Long,
       b: Seq[Bit]
   ): Try[(Packet, Seq[Bit])] = Try {
     // TODO: cleanup
@@ -59,13 +74,15 @@ object Year2021Day16 {
     val afterN = b.drop(nlen + 1)
     val result =
       if (b.head == 0)
-        parsePackets(afterN.take(n)).map(ops => (ops, afterN.drop(n)))
+        parsePackets(afterN.take(n.toInt)).map(ops =>
+          (ops, afterN.drop(n.toInt))
+        )
       else parsePacketsN(afterN, n)
     val (ops, remaining) = result.get
     (Operator(version, typ, ops), remaining)
   }
 
-  private def parseHeader(b: Seq[Bit]): Try[(Int, Int, Seq[Bit])] = Try {
+  private def parseHeader(b: Seq[Bit]): Try[(Long, Long, Seq[Bit])] = Try {
     val Seq(v1, v2, v3) = b.take(3)
     val Seq(t1, t2, t3) = b.drop(3).take(3)
     (v1 << 2 | v2 << 1 | v3, t1 << 2 | t2 << 1 | t3, b.drop(6))
@@ -99,29 +116,29 @@ object Year2021Day16 {
     }
   }
 
-  def sumVersions(packet: Packet): Int = packet match {
+  def sumVersions(packet: Packet): Long = packet match {
     case Literal(version, _)       => version
     case Operator(version, _, ops) => version + ops.map(sumVersions).sum
   }
 
-  def evaluate(packet: Packet): Int = packet match {
-    case Literal(version, lit) => lit
-    case Operator(version, typ, operands) => {
-      val ops = operands.map(evaluate)
-      typ match {
-        case 0 => ops.sum
-        case 1 => ops.reduce((a, b) => a * b)
-        case 2 => ops.min
-        case 3 => ops.max
-        case 5 => ops.reduce((a, b) => if (a > b) 1 else 0)
-        case 6 => ops.reduce((a, b) => if (a < b) 1 else 0)
-        case 7 => ops.reduce((a, b) => if (a == b) 1 else 0)
-      }
+  def evaluate(packet: Packet): Long =
+    packet match {
+      case Literal(version, lit) => lit
+      case Operator(version, typ, operands) =>
+        (typ, operands.map(evaluate)) match {
+          case (0, op1 :: ops) => ops.foldLeft(op1)((acc, op) => acc + op)
+          case (1, op1 :: ops) => ops.foldLeft(op1)((acc, op) => acc * op)
+          case (2, ops)        => ops.min
+          case (3, ops)        => ops.max
+          case (5, (left :: right :: Nil)) => if (left > right) 1 else 0
+          case (6, (left :: right :: Nil)) => if (left < right) 1 else 0
+          case (7, (left :: right :: Nil)) => if (left == right) 1 else 0
+        }
     }
-  }
 
   def main(args: Array[String]): Unit = {
     val packet = parse(decode(Source.fromFile(args(0))).get).get
     println(sumVersions(packet))
+    println(evaluate(packet))
   }
 }
